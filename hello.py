a296f4251e4c72f17189db04c5ad39fbe3f1e970
Patrick Meehan <G00364732@gmit.ie>
2018-02-19 19:20:06 +0000

    added some new programs

---
+++ hello.py
@@ -0,0 +1,15 @@
+# Patrick Meehan 2018-02-16
+# testing various commands
+
+first_name = "patrick "
+last_name = "meehan"
+full_name = (first_name + last_name)
+print (full_name)
+
+
+teams =["rangers","flyers","devils"]
+print (teams[0])
+
+
+import random
+print(random.randint(0,9))
