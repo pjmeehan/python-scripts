5b3caafd2bc73d9e723e8c64c46102d828ae1753
Patrick Meehan <G00364732@gmit.ie>
2018-03-20 14:44:44 +0000

    updated

--- euler5.py
+++ euler5.py
@@ -1,5 +1,18 @@
 # Patrick Meehan 2018-02-19
 # exercise 4, euler problem 5
+
+# Patrick Meehan 2018-02-19
+# exercise 4, euler problem 5
+
+ 
+#original attempt
+# s = 2520 # we know 1 thru 10 lowest number is 2520, so we start with that number and eliminate the need of going thru range 1 thru 10 again.
+
+# for i in range(11, 21, 20):  ## we know 1 thru 10, so start range with 11 thru 21 in steps of 20 because we know the number we are looking for must be divided by 20
+#  if s % x[i] == 0:
+#    print (s)
+# s = s + 1  
+
 # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
  
