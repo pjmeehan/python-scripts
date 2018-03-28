b4a11d7bf4ca9c5495d6ac1b896f3ed1c3dace90
Patrick Meehan <G00364732@gmit.ie>
2018-03-28 13:23:29 +0100

    added comment

--- collatz.py
+++ collatz.py
@@ -2,6 +2,10 @@
 # testing github repository update
 # Collatz conjecture (https://en.wikipedia.org/wiki/Collatz_conjecture)
 
+# basic premise : start with any positive integer n. Then each term is obtained from the previous term as follows:
+# if the previous term is even, the next term is one half the previous term. Otherwise, the next term is 3 times
+# the previous term plus 1. The conjecture is that no matter what value of n, the sequence will always reach 1.
+
 # set up script to allow user to input a number
 
 x = int(input("Please enter an integer: "))
