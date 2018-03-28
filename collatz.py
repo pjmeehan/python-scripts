bfe4075762c00597e9553636406c504cd5a55210
Patrick Meehan <G00364732@gmit.ie>
2018-03-28 13:48:09 +0100

    added comments

--- collatz.py
+++ collatz.py
@@ -10,11 +10,25 @@
 
 x = int(input("Please enter an integer: "))
 
+# run a while loop where x > 1 so that the script will only run if a positive integer is input.
+
 while x > 1: 
+
+# the print (x) statement will ask the user to input their number and will print that number.
+
     print (x)     
+
+# using an if/else statement. start with a modulo to test if integer is even. if it is,
+# it loops until an odd integer is found and defers top the else statment and runs that calculation for an odd integer.
+# the loop will continue until it reaches 1.
+
     if x % 2 == 0:
-        x = (int(x/2))
+                x = (int(x/2))
     else:
         x = (int((x*3)+1))
+
+# using an else statement with the while statement. if the integer is not positive or x < 1, the else statement
+# simply tells the script to just print what ever number was input. ie -8 will just print -8. 
+
 else:
     print (int(x)) 
