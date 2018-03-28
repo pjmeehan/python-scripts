986411d4c18a73a8661e434a311359ee9ecb6a22
Patrick Meehan <G00364732@gmit.ie>
2018-03-28 14:03:02 +0100

    added comments

--- factorial.py
+++ factorial.py
@@ -1,14 +1,32 @@
 # Patrick Meehan 13-03-2018
 # exercise 6 - script for returning the factorial of a given number
 
+# factorial definition (as per google dictionary) : the product of an integer and all the integers below it.
 
 
-def factorial(n): #n is my input of the function
+# n is my input of the function.
+# the def function lets us name a function where we can use later just by using the named function rather than
+# inputting the whole script again.
+
+def factorial(n): 
     answer = 1
-    while n >= 1: #as long as n is greater or equal to 1, the loop continues
-        answer = answer * n  #answer is the place number while running thru the loop. it changes to a new varaible every time
-        n = n - 1 #this will decrease n until it equals 1, then the loop will stop
-    return answer # this is the output of the function
+
+# create a loop with a 'while' statement to run through the given integer and all the integers below it.
+# as long as n is greater or equal to 1, the loop will continue run.
+
+    while n >= 1: 
+
+# answer is the place number while running thru the loop. it changes to a new varaible every time.
+
+        answer = answer * n  
+
+# this will decrease n until it equals 1, then the loop will stop.
+
+        n = n - 1 
+
+# this is the output of the function.
+
+    return answer 
 
 n = int(input("Please enter an integer: ",))   #allows for the input of a positive integer
 print(factorial(n)) #will return the factorial based on the above script that I created for factorial(n)
